a9be50a51387daf036161b64902b53fcd7631e1c
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2023-08-10 19:03:29 +0200

    Added comments, erased unnuse code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
-function displayPokemon() {
+const pokemonRepository = (function () {
   const pokemonList = [
     { name: "Bulbasaur", height: 7, weight: 6.9, types: ["grass", "poison"] },
     { name: "Charmander", height: 6, weight: 9, types: ["fire"] },
     { name: "Squirtle", height: 0.5, weight: 8.5, types: ["water"] },
   ];
 
-  pokemonList.forEach(function (pokemon) {
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+function displayPokemon() {
+  document.getElementById("output").innerHTML = "";
+
+  pokemonRepository.getAll().forEach(function (pokemon) {
     const name = pokemon.name;
     const height = pokemon.height;
     const weight = pokemon.weight;
