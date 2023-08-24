5e760b6004082d5169654f82611cd4bff4c45846
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2023-08-24 15:44:53 +0200

    added new CSS style for buttons, added DOM manipulation and event listeners code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,44 @@
 const pokemonRepository = (function () {
   var pokemonList = [
+    { name: "Pikachu", height: 2, weight: 13, types: ["electric"] },
     { name: "Bulbasaur", height: 7, weight: 6.9, types: ["grass", "poison"] },
     { name: "Charmander", height: 6, weight: 9, types: ["fire"] },
     { name: "Squirtle", height: 0.5, weight: 8.5, types: ["water"] },
   ];
+
+  function addListItem(pokemon) {
+    const listItem = document.createElement('li');
+    const button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+    listItem.appendChild(button);
+
+
+    let infoShown = false;
+
+
+    button.addEventListener('click', function () {
+
+      const pokemonInfo = document.getElementById('pokemon-info');
+
+      if (infoShown) {
+
+        pokemonInfo.innerHTML = '';
+      } else {
+        // Show the info
+        pokemonInfo.innerHTML = '<h3>' + pokemon.name + '</h3>'
+          + '<p>Height: ' + pokemon.height + '</p>'
+          + '<p>Weight: ' + pokemon.weight + '</p>'
+          + '<p>Type: ' + pokemon.types.join(", ") + '</p>';
+      }
+
+
+      infoShown = !infoShown;
+    });
+
+    let pokemonListElement = document.querySelector('.pokemon-list');
+    pokemonListElement.appendChild(listItem);
+  }
 
   function getAll() {
     return pokemonList;
@@ -11,52 +46,12 @@ const pokemonRepository = (function () {
 
   return {
     getAll: getAll,
+    addListItem: addListItem,
   };
 })();
 
-// Function to add a list item for a Pokémon
-function addListItem(pokemon) {
-  var outputDiv = document.getElementById("output");
-  var listItem = document.createElement("div");
-  listItem.innerHTML = '<h3>' + pokemon.name + '</h3>'
-                     + '<p>Height: ' + pokemon.height + '</p>'
-                     + '<p>Weight: ' + pokemon.weight + '</p>'
-                     + '<p>Type: ' + pokemon.types.join(", ") + '</p>';
-  listItem.addEventListener("click", function() {
-    console.log("Clicked Pokémon: " + pokemon.name);
-  });
-  outputDiv.appendChild(listItem);
-}
+console.log(pokemonRepository.getAll());
 
-// Function to search for a Pokémon by name
-function searchPokemon() {
-  var searchValue = document.getElementById("search-input").value.trim().toLowerCase();
-  var outputDiv = document.getElementById("output");
-  outputDiv.innerHTML = ""; // Clear previous details
-
-  var pokemon = pokemonRepository.getAll().find(function(p) {
-    return p.name.toLowerCase() === searchValue;
-  });
-
-  if (pokemon) {
-    addListItem(pokemon);
-  } else {
-    outputDiv.innerHTML = "<p>Pokémon not found!</p>";
-  }
-}
-
-// Function to display all Pokémon
-function showAllPokemon() {
-  var outputDiv = document.getElementById("output");
-  outputDiv.innerHTML = ""; // Clear previous details
-
-  var allPokemon = pokemonRepository.getAll();
-
-  allPokemon.forEach(function(pokemon) {
-    addListItem(pokemon);
-  });
-}
-
-// Attach functions to buttons
-document.getElementById("search-button").addEventListener("click", searchPokemon);
-document.getElementById("show-all-button").addEventListener("click", showAllPokemon);
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
