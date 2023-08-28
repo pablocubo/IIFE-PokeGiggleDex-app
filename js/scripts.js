2353213f53b981fce71f81577f24452363ce31d5
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2023-08-28 10:15:48 +0200

    Added API and updtaded Visual in CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,6 @@
-const pokemonRepository = (function () {
-  var pokemonList = [
-    { name: "Pikachu", height: 2, weight: 13, types: ["electric"] },
-    { name: "Bulbasaur", height: 7, weight: 6.9, types: ["grass", "poison"] },
-    { name: "Charmander", height: 6, weight: 9, types: ["fire"] },
-    { name: "Squirtle", height: 0.5, weight: 8.5, types: ["water"] },
-  ];
+var pokemonRepository = (function () {
+  var pokemonList = [];
+  var infoShown = false;
 
   function addListItem(pokemon) {
     const listItem = document.createElement('li');
@@ -13,26 +9,25 @@ const pokemonRepository = (function () {
     button.classList.add('pokemon-button');
     listItem.appendChild(button);
 
-
-    let infoShown = false;
-
-
     button.addEventListener('click', function () {
-
       const pokemonInfo = document.getElementById('pokemon-info');
-
       if (infoShown) {
-
         pokemonInfo.innerHTML = '';
       } else {
         // Show the info
-        pokemonInfo.innerHTML = '<h3>' + pokemon.name + '</h3>'
-          + '<p>Height: ' + pokemon.height + '</p>'
-          + '<p>Weight: ' + pokemon.weight + '</p>'
-          + '<p>Type: ' + pokemon.types.join(", ") + '</p>';
+        pokemonInfo.innerHTML = '<div class="pokemon-details">' +
+          '<h3>' + pokemon.name + '</h3>' +
+          '<img src="' + pokemon.imgUrl + '">' +
+          '<p>Height: ' + pokemon.height + '</p>' +
+          '<p>Weight: ' + pokemon.weight + '</p>' +
+          '<p>Types: ' + pokemon.types.join(', ') + '</p>' +
+          '<p>Stats: ' + JSON.stringify(pokemon.stats) + '</p>' +
+          '<p>Sprites: ' + JSON.stringify(pokemon.sprites) + '</p>' +
+          '<p>Moves: ' + JSON.stringify(pokemon.moves) + '</p>' +
+          '<p>Species: ' + JSON.stringify(pokemon.species) + '</p>' +
+          '<p>Evolution Chain: ' + JSON.stringify(pokemon.evolutionChain) + '</p>' +
+          '</div>';
       }
-
-
       infoShown = !infoShown;
     });
 
@@ -44,14 +39,57 @@ const pokemonRepository = (function () {
     return pokemonList;
   }
 
+  function loadList() {
+    return fetch('https://pokeapi.co/api/v2/pokemon/')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        data.results.forEach(function (item) {
+          var pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          pokemonList.push(pokemon);
+        });
+      })
+      .catch(function (error) {
+        console.error('Error loading Pokémon list', error);
+      });
+  }
+
+  function loadDetails(pokemon) {
+    var url = pokemon.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        // Update imgUrl to the front sprite image URL
+        pokemon.imgUrl = details.sprites.front_default;
+        pokemon.height = details.height;
+        pokemon.weight = details.weight;
+        pokemon.types = details.types;
+        // ... (other properties)
+      })
+      .catch(function (error) {
+        console.error('Error loading Pokémon details', error);
+      });
+  }
+
   return {
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
   };
 })();
 
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.loadDetails(pokemon).then(function () {
+      pokemonRepository.addListItem(pokemon);
+    });
+  });
 });
+
